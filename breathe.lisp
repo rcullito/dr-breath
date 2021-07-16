7796cd7c89fd3d6b6b2d70117abd97a8005e0d49
Rob Culliton <rob.culliton@gmail.com>
2021-07-16 09:30:48 -0400

    good, now we can just do without including the newline characters in our stream

--- breathe.lisp
+++ breathe.lisp
@@ -6,14 +6,16 @@
     ;; lines in the corpus that will not have a gap
     (subseq txt 0 end-of-left-column)))
 
-;; (subseq "howdy good sir" 0 nil)
-
 (defun right-column (txt delim)
   (let ((adjustment (length delim))
         (backwards-search-position (search delim txt :from-end :backward)))
     (when backwards-search-position
       (let ((beginning-of-right-column (+ backwards-search-position adjustment)))
-            (subseq txt beginning-of-right-column)))))
+        (subseq txt beginning-of-right-column)))))
+
+(defparameter *left-stream* (make-string-output-stream))
+(defparameter *right-stream* (make-string-output-stream))
+
 
 (with-open-file (my-stream "sample.txt" :direction :input)
   (loop
@@ -25,7 +27,10 @@
          ;; we can't alternate per line, we need to send each to a different stream and then join
          ;; those at the end of a page
          (when (not (equal "" left-part))
-           (print left-part))
+           (print left-part *left-stream*))
          (when right-part
-           (print right-part)))))
+           (print right-part *right-stream*)))))
 
+
+
+(get-output-stream-string *left-stream*)
