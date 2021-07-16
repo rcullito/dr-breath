517f6b63f74d8ae4cee88e09042c392638cc7a70
Rob Culliton <rob.culliton@gmail.com>
2021-07-16 08:35:03 -0400

    cleaning up the loop's for and until conditions rather than relying on an inline when

--- breathe.lisp
+++ breathe.lisp
@@ -14,9 +14,10 @@
 (with-open-file (my-stream "sample.txt" :direction :input)
   ;; todo split on blank space, and acc the left in one var and the right in another
   (loop
-    (let ((current-line (read-line my-stream)))
-     (when current-line
+    for current-line = (read-line my-stream nil 'eof)
+    until (eq current-line 'eof)
+    do
        (let* ((first-line (left-column current-line *delimeter*))
               (second-line (right-column current-line *delimeter*)))
          (when second-line
-           (print second-line)))))))
+           (print second-line)))))
