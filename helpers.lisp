c6da4e6e1775debeb1c0f56ee6b5074c1373d13e
Rob Culliton <rob.culliton@gmail.com>
2021-07-25 15:03:30 -0400

    delete predicates and params we know we don't want. deal with the consequences tomorrow

--- helpers.lisp
+++ helpers.lisp
@@ -1,5 +1,3 @@
-(defparameter *line-cutoff* 65)
-
 (defun page-heading-p (txt)
   (search "  Dr. Breath  " txt))
 
@@ -18,16 +16,6 @@
 (defvar odd-page-num-p (make-page-predicate #'oddp))
 (defvar even-page-num-p (make-page-predicate #'evenp))
 
-(defun first-half (text)
-  (subseq text 0 *line-cutoff*))
-
-(defun short-line-p (text)
-  (< (length text) *line-cutoff*))
-
-(defvar long-line-p (complement #'short-line-p))
-
-(defun false-start-p (text delim)
-  (equal delim (subseq text 0 (length delim))))
 
 (defmacro file->file (input-file output-file &body body)
   `(with-open-file (output-stream ,output-file :direction :output :if-exists :supersede)
